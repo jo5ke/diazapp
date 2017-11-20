b0048664591102a73b32cfced69f1b8347b84799
Josif Kostić <joskekostic@gmail.com>
2017-11-21 00:23:26 +0100

    Admin dashboard set-up

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,9 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
+
 
 mix.styles([
     'resources/assets/css/libs/blog-post.css',
@@ -24,10 +26,10 @@ mix.styles([
 ], 'public/css/libs.css');
 
 mix.scripts([
+    'resources/assets/js/libs/jquery.js',
     'resources/assets/js/libs/bootstrap.js',
     'resources/assets/js/libs/metisMenu.js',
     'resources/assets/js/libs/sb-admin-2.js',
-    'resources/assets/js/libs/jquery.js',
     'resources/assets/js/libs/scripts.js'
 
 ], 'public/js/libs.js');
